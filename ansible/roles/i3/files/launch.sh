de337ce1abcec74db4709406a3b0c5612c26d449
antisa <antisa@pm.me>
2021-11-22 16:43:44 +0100

    ansible: update i3 configuration

--- ansible/roles/i3/files/launch.sh
+++ ansible/roles/i3/files/launch.sh
@@ -7,7 +7,16 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# Load polybar on every monitor
+#if type "xrandr"; then
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  MONITOR=$m polybar --reload bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+done
+#else
+#    polybar --reload bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+#fi
+
+#polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 #polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
